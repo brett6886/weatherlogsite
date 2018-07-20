b535fc54b3a0da06cfe104bcda9997f197078296
brett6886 <brett6886@msn.com>
2018-07-20 14:01:44 -0400

    Aesthetics, Input Validation, and Database Update
    Changed some aspects of the appearance, adjusted the ValidateDT function in weatherScript.js to handle incorrect outputs, and replaced date and time columns in mysql database with a single datetime column

--- weatherdisplay/static/js/weatherScript.js
+++ weatherdisplay/static/js/weatherScript.js
@@ -1,9 +1,14 @@
+//*********************************************************
+
+emptyData = false;
+
 //appearance during data fetch
 $(document).ajaxStart(function(){
 	$("#wait").css("display", "block");
 	$("#statTable").hide();
 	$("#timeseries-title").hide();
 	$("#histogram-title").hide();
+	document.getElementById("err").innerHTML = "";
 	//reset timeseries plots
 	Plotly.purge(hPlot);
 	Plotly.purge(tPlot);
@@ -14,42 +19,49 @@ $(document).ajaxStart(function(){
 	Plotly.purge(pHist);
 });
 
+
+
+
 //normal appearance
 $(document).ajaxComplete(function(){
 	$("#wait").css("display", "none");
-	$("#statTable").show();
+	if(!emptyData){ $("#statTable").show();}
 	$("#histogram-title").show();
 	$("#timeseries-title").show();
 });
 
 
+
+
 //gets and converts datetime string inputs into proper format ('yyyy-mm-dd hh:mm:ss')
 function getValidDT(){
-	
-	//read datetimes from input boxes
-	var startString = document.getElementById("start").value;
-	var endString = document.getElementById("end").value;
-	
-	//get current datetime -> (yyyy-mm-dd hh:mm:ss) format
+
+    //date of initial sensor reading (June 08, 2018)
+    sensorBegin = new Date("2018-06-08 12:41:00");
+
+    //get current datetime
 	var now = new Date();
-	var nowMonth = (((now.getMonth()) < 9 ? "0" : "") + ((now.getMonth()) + 1));
-	var nowDate = (((now.getDate()) < 9 ? "0" : "") + (now.getDate()));
-	var nowHrs = (((now.getHours()) < 9 ? "0" : "") + (now.getHours()));
-	var nowMins = (((now.getMinutes()) < 9 ? "0" : "") + (now.getMinutes()));
-	var nowSecs = (((now.getSeconds()) < 9 ? "0" : "") + (now.getSeconds()));
-	var nowString = now.getFullYear() + "-" + nowMonth + "-" + nowDate + " " + nowHrs + ":" + nowMins + ":" + nowSecs;
-	
+    nowString = inputFormat(now);
+
+	//get starting datetime
+	var startString = document.getElementById("start").value;
+
+	//get ending datetime
+	var endString = document.getElementById("end").value;
+
+
 	/**handle empty strings**/
-	//set starting datetime to earliest data point if empty
-	if (startString == "") {
-		startString = "2018-06-07 00:00:00";
-	}
-	//set ending datetime to current if empty
-	if (endString == "") {
-		endString = nowString;
-	}
-	
-	
+	if(!(startString.includes("T"))){
+        alert("**Invalid Start Date");
+		return null;
+    }
+    if(!(endString.includes("T"))){
+        alert("**Invalid End Date");
+		return null;
+    }
+
+
+
 	/**handle boundary and order problems**/
 	var start = new Date(startString);
 	var end = new Date(endString);
@@ -58,41 +70,52 @@ function getValidDT(){
 		alert("**End time must come after start time");
 		return null;
 	}
+	else{
+	    //change start date to initial sensor reading if too far in the past
+	    if(start < sensorBegin){
+	        alert("**No data collected prior to 06/08/2018 12:41 PM");
+	        start = sensorBegin;
+	        document.getElementById("start").value = inputFormat(sensorBegin);
 
-	//date/time must be AFTER initial sensor data (June 7, 2018)
-	if (start < new Date("2018-06-07 00:00:00")) {
-		startString = "2018-06-07 00:00:00";
+	        //change end date to today if too far in the future
+	        if(end < sensorBegin){
+    	        end = now;
+        		document.getElementById("end").value = nowString;
+	        }
+	    }
+
+	    //set end to current time if it's in the future
+	    if (end > now) {
+    		end = now;
+    		document.getElementById("end").value = nowString;
+	    }
+
+	    //get at least 2 data points
+        if (((end - start)/60000.0) < 10){
+            end = new Date((start.getTime() + 10*60000));
+            document.getElementById("end").value = inputFormat(end);
+            console.log("new end time: " + end.toString());
+        }
 	}
-	//date/time must be BEFORE current date/time
-	if (end > now) {
-		endString = nowString;
-	}
-	
-	
+
+
 	/**Finalize datetime strings -> (yyyy-mm-dd hh:mm:ss) format**/
-	//finalize start datetime input 
-	var startMonth = (((start.getMonth()) < 9 ? "0" : "") + ((start.getMonth()) + 1));
-	var startDate = (((start.getDate()) < 9 ? "0" : "") + ((start.getDate())));
-	var startHrs = (((start.getHours()) < 9 ? "0" : "") + ((start.getHours())));
-	var startMins = (((start.getMinutes()) < 9 ? "0" : "") + ((start.getMinutes())));
-	var startSecs = (((start.getSeconds()) < 9 ? "0" : "") + ((start.getSeconds())));
-	startString = start.getFullYear() + "-" + startMonth + "-" + startDate + " " + startHrs + ":" + startMins + ":" + startSecs;
+	//finalize start datetime input
+	startString = djangoFormat(start);
 	//finalize end datetime input
-	var endMonth = (((end.getMonth()) < 9 ? "0" : "") + ((end.getMonth()) + 1));
-	var endDate = (((end.getDate()) < 9 ? "0" : "") + ((end.getDate())));
-	var endHrs = (((end.getHours()) < 9 ? "0" : "") + ((end.getHours())));
-	var endMins = (((end.getMinutes()) < 9 ? "0" : "") + ((end.getMinutes())));
-	var endSecs = (((end.getSeconds()) < 9 ? "0" : "") + ((end.getSeconds())));
-	endString = end.getFullYear() + "-" + endMonth + "-" + endDate + " " + endHrs + ":" + endMins + ":" + endSecs;
-	
-	
+	endString = djangoFormat(end);
+
+
 	return [startString, endString];
 } //end validateDT
 
 
+
+
+
 //fetches data from database
 function getdata(){
-	
+
 	//real values
 	var datetimes = [];
 	var humidities = [];
@@ -103,14 +126,11 @@ function getdata(){
 	//stats table values
 	var hAvg, tAvg, pAvg;
 	var hStdv, tStdv, pStdv;
-	
+
 
 	/**get valid start and end datetimes**/
 	var validated = getValidDT();
-	//return failure if dates are out of order
-	if (validated == null){
-		return null;
-	}
+	if (validated == null) { return null; }
 	var startString = validated[0];
 	var endString = validated[1];
 
@@ -122,7 +142,24 @@ function getdata(){
 		data: {'start': startString, 'end':endString},
 		url: "getdata/",
 		success: function(result){
-			//real values
+
+
+		    //set error message if query returned empty
+		    if (result.datetimes === null){
+		        console.log("***Data query returned empty");
+		        emptyData = true;
+		        document.getElementById("err").innerHTML = "No data available for that time period";
+		        return null;
+		    }
+		    //log number of data points fetched
+		    else{
+                console.log("***number of data points fetched: " + result.datetimes.length);
+		        emptyData = false;
+		        document.getElementById("err").innerHTML = "";
+		    }
+
+
+			//get real values
 			datetimes = result.datetimes.slice(0);
 			humidities = result.humidities.slice(0);
 			temps = result.temps.slice(0);
@@ -132,7 +169,7 @@ function getdata(){
 			let {mean: hAvg, deviation: hStdv} = stats(humidities);
 			let {mean: tAvg, deviation: tStdv} = stats(temps);
 			let {mean: pAvg, deviation: pStdv} = stats(pressures);
-			
+
 			//set humidity table values
 			document.getElementById("hAvg").innerHTML = hAvg + ' %';
 			document.getElementById("hStdv").innerHTML = hStdv + ' %';
@@ -142,23 +179,24 @@ function getdata(){
 			//set pressure table values
 			document.getElementById("pAvg").innerHTML = pAvg + ' hPa';
 			document.getElementById("pStdv").innerHTML = pStdv + ' hPa';
-			
-			
-			
+
+
 			//draw plots
-			timeplot(datetimes, humidities, "Humidity", "percent", "hPlot"); 
-			timeplot(datetimes, temps, "Temperature", "celcius", "tPlot"); 
-			timeplot(datetimes, pressures, "Pressure", "hPa", "pPlot");
-			hist(humidities, "Humidity", "percent", "hHist"); 
-			hist(temps, "Temperature", "celcius", "tHist"); 
-			hist(pressures, "Pressure", "hPa", "pHist");
-		}, 
+			timeplot(datetimes, humidities, "percent", "hPlot");
+			timeplot(datetimes, temps, "celcius", "tPlot");
+			timeplot(datetimes, pressures, "hPa", "pPlot");
+			hist(humidities, "percent", "hHist");
+			hist(temps, "celcius", "tHist");
+			hist(pressures, "hPa", "pHist");
+		},
 		error: function(jqxhr, stat, exception){
 			alert("failed to access data");
 		}
 	});//end ajax()
 
 }//end getdata()
+
+
 
 
 //returns mean and standard deviation of array
@@ -169,13 +207,13 @@ function stats(arr) {
 	var variance = 0.0;
 	var v1 = 0.0;
 	var v2 = 0.0;
-	
+
 	//get mean
 	arr.map(function(data) {
 		sum+=data;
 	});
 	mean = sum / n;
-	
+
 	//get stdv
 	if (n > 1) {
 		for (var i = 0; i < n; i++) {
@@ -185,11 +223,11 @@ function stats(arr) {
 
 		v2 = v2 * v2 / n;
 		variance = (v1 - v2) / (n-1);
-		
-		if (variance < 0){ 
-			variance = 0; 
+
+		if (variance < 0){
+			variance = 0;
 		}
-		
+
 		stddev = Math.sqrt(variance);
 	}
 
@@ -200,15 +238,17 @@ function stats(arr) {
 }; //end stats
 
 
-//draws time series plot with real and simulated values
-function timeplot(dates, values, Title, units, div){
-	
+
+
+//draws time series plot
+function timeplot(dates, values, units, div){
+
 	var divName = document.getElementById(div);
-	
+
 	var trace = [{x: dates, y: values, type: 'bar', name: 'Real data'}];
-	
+
 	var layout = {
-		title: Title,
+		title: "Time Series",
 		xaxis: {
 			title: 'time',
 			titlefont: {
@@ -226,41 +266,43 @@ function timeplot(dates, values, Title, units, div){
 				size: 18,
 				color: '#7f7f7f'
 			},
-			range: [Math.min.apply(null, values), Math.max.apply(null, values)]
+			range: [0, Math.max.apply(null, values)]
 		}
 	};
 
 	//time series
-	Plotly.newPlot(divName, trace, layout); 
+	Plotly.newPlot(divName, trace, layout);
 
 }//end timeplot
 
 
-//draws time series plot with real and simulated values
-function hist(values, Title, units, div){
-	
+
+
+//draws time series plot
+function hist(values, units, div){
+
 	var divName = document.getElementById(div);
-	
-	var trace = [{x: values, 
-		type: 'histogram', 
+
+	var trace = [{x: values,
+		type: 'histogram',
 		marker: {
 			color: "#ff3030"
 		}
 	}];
-	
+
 	var layout = {
-		bargap: 0.05, 
-		bargroupgap: 0.2, 
-		barmode: "overlay", 
-		title: Title, 
+		bargap: 0.05,
+		bargroupgap: 0.2,
+		barmode: "overlay",
+		title: "Histogram",
 		xaxis: {
-			title: units, 
+			title: units,
 			titlefont: {
 				family: 'Courier New, monospace',
 				size: 18,
 				color: '#7f7f7f'
 			}
-		}, 
+		},
 		yaxis: {
 			title: "Quantity",
 			titlefont: {
@@ -269,27 +311,60 @@ function hist(values, Title, units, div){
 				color: '#7f7f7f'
 			}
 		},
-		range: [Math.min.apply(null, values), Math.max.apply(null, values)]
+		range: [0, Math.max.apply(null, values)]
 	};
-	
+
 	//histogram
 	Plotly.newPlot(divName, trace, layout);
 
 }//end hist
 
 
-/**RUN AT PAGE STARTUP*****************************************************************************************/
-//initialize start and end date/time inputs
-document.getElementById("start").value = "2018-06-12T00:00";
-t0 = new Date();
-var nowMonth = (((t0.getMonth()) < 9 ? "0" : "") + ((t0.getMonth()) + 1));
-var nowDate = (((t0.getDate()) < 9 ? "0" : "") + (t0.getDate()));
-var nowHrs = (((t0.getHours()) < 9 ? "0" : "") + (t0.getHours()));
-var nowMins = (((t0.getMinutes()) < 9 ? "0" : "") + (t0.getMinutes()));
-var nowString = t0.getFullYear() + "-" + nowMonth + "-" + nowDate + "T" + nowHrs + ":" + nowMins;
-document.getElementById("end").value = nowString;
 
-//get data on page startup
-getdata();
+
+//get input-formatted string from date (aka the format accepted by the user input box)
+function inputFormat(date){
+
+    var dateMonth = ((date.getMonth() < 9 ? "0" : "") + (date.getMonth() + 1));
+    var dateDate = ((date.getDate() < 9 ? "0" : "") + date.getDate());
+    var dateHrs = ((date.getHours() < 9 ? "0" : "") + date.getHours());
+    var dateMins = ((date.getMinutes() < 9 ? "0" : "") + date.getMinutes());
+    var formatted = date.getFullYear() + "-" + dateMonth + "-" + dateDate + "T" + dateHrs + ":" + dateMins;
+
+    return formatted;
+}
+
+
+
+
+//get django-formatted string from date
+function djangoFormat(date){
+
+    var dateMonth = (((date.getMonth()) < 9 ? "0" : "") + ((date.getMonth()) + 1));
+	var dateDate = (((date.getDate()) < 9 ? "0" : "") + ((date.getDate())));
+	var dateHrs = (((date.getHours()) < 9 ? "0" : "") + ((date.getHours())));
+	var dateMins = (((date.getMinutes()) < 9 ? "0" : "") + ((date.getMinutes())));
+	var dateSecs = (((date.getSeconds()) < 9 ? "0" : "") + ((date.getSeconds())));
+	var formatted = date.getFullYear() + "-" + dateMonth + "-" + dateDate + " " + dateHrs + ":" + dateMins + ":" + dateSecs;
+
+    return formatted;
+}
+
+
+
+
+/**RUN AT PAGE STARTUP*****************************************************************************************/
+//stat table and error message initially not visible
+$("#statTable").hide();
+document.getElementById("err").innerHTML = "";
+
+//set the start-date-input to first sensor reading date
+document.getElementById("start").value = "2018-06-08T12:41";
+
+//set end-date-input to current date
+endt = new Date();
+document.getElementById("end").value = inputFormat(endt);
+
+//fetch data
+window.onload = getdata;
 /**************************************************************************************************************/
-		
--- weatherdisplay/views.py
+++ weatherdisplay/views.py
@@ -2,98 +2,95 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import mysql.connector as conn
 import sshtunnel
-import time
 import weatherdisplay.config as conf
-
-
 
 
 #home page view
 def home(request):
-    
-    return render(request, 'weatherdisplay/home.html', {})
 
+    return render(request, 'weatherdisplay/home.html', {})
 
 
 
 
 #data fetch view
 def getdata(request):
-    
+
     data = {} #holds all data
     datetimes = []
     humidities = []
     temps = []
     pressures = []
-    
-    #get date/time range from user input
+
+    #get date/time range from user input (format 'yyyy-mm-dd hh:mm:ss')
     start = request.POST.get('start')
     end = request.POST.get('end')
-    #split dates and times
-    startDate, startTime = start.split()
-    endDate, endTime = end.split()
+    print('\n***start date:', start)
+    print('***end date:', end)
 
     #set max timeout
     sshtunnel.SSH_TIMEOUT = 5.0
     sshtunnel.TUNNEL_TIMEOUT = 5.0
 
 
-    #keep fetching until successful
-    while len(datetimes) == 0 or len(humidities) == 0 or len(temps) == 0 or len(pressures) == 0:
-
-        #create ssh tunnel to cloud server
-        with sshtunnel.SSHTunnelForwarder(
-            ("ssh.pythonanywhere.com"),
-            ssh_password = conf.ssh_password,
-            ssh_username = conf.ssh_username,
-            remote_bind_address=('iambrett.mysql.pythonanywhere-services.com', 3306)
-        ) as tunnel:
-            #connect to cloud database
-            db = conn.connect(
-                host = "127.0.0.1",
-                user = conf.user,
-                password = conf.password,
-                port = tunnel.local_bind_port,
-                database = "iambrett$weatherlog")
+    #create ssh tunnel to cloud server
+    with sshtunnel.SSHTunnelForwarder(
+        ("ssh.pythonanywhere.com"),
+        ssh_password = conf.ssh_password,
+        ssh_username = conf.ssh_username,
+        remote_bind_address=('iambrett.mysql.pythonanywhere-services.com', 3306)
+    ) as tunnel:
+        #connect to cloud database
+        db = conn.connect(
+            host = "127.0.0.1",
+            user = conf.user,
+            password = conf.password,
+            port = tunnel.local_bind_port,
+            database = "iambrett$weatherlog")
 
 
 
-            #read data from database
-            c = db.cursor()
-            qStr = "SELECT * FROM weatherVars WHERE date >= \'" + startDate \
-                + "\' AND date <= \'" + endDate \
-                + "\' AND time >= \'" + startTime \
-                + "\' AND time <= \'" + endTime + "\';"  
-            query2 = (qStr)
-            c.execute(query2)
-            for line in c:
-                datetimes.append('{} {}'.format(line[1], line[2]))
-                humidities.append(line[3])
-                temps.append(line[4])
-                pressures.append(line[5])
-            c.close()
-            
-            
-            #add all lists to dictionary (first 4 values in database are just 0)
-            data['datetimes'] = datetimes[4:]
-            data['humidities'] = humidities[4:] 
-            data['temps'] = temps[4:]
-            data['pressures'] = pressures[4:]
-            
+        #read data from database
+        c = db.cursor()
+        qStr = "SELECT * FROM weatherVars WHERE datetimes >= \'" + start + "\' AND datetimes <= \'" + end + "\';"
+        query2 = (qStr)
+        c.execute(query2)
+        for line in c:
+            datetimes.append('{}'.format(line[1]))
+            humidities.append(line[2])
+            temps.append(line[3])
+            pressures.append(line[4])
+        c.close()
 
-            #close database connections
-            db.close()
-            db = None
-            ##end with-as
-    
-        
-        #allow half-second break between fetch attempts
-        time.sleep(0.5)
-        ##end while loop
-    
-    
-    
+
+        #return Nothing if no sufficient data found in database
+        print('***Number of datapoints fetched:', len(datetimes), '\n')
+        if(len(datetimes) < 2):
+            data['datetimes'] = None
+            data['humidities'] = None
+            data['temps'] = None
+            data['pressures'] = None
+            return JsonResponse(data)
+
+
+        #add all lists to dictionary
+        data['datetimes'] = datetimes
+        data['humidities'] = humidities
+        data['temps'] = temps
+        data['pressures'] = pressures
+
+
+        #close database connections
+        db.close()
+        db = None
+        ##end with-as
+
+
+
+
     #return all data as json object
     return JsonResponse(data)
+
+
 
 
